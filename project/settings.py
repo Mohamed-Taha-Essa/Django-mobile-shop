c7b0b1a3fca1c1ec73ce29d710573c000f8c419d
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-05-24 00:24:34 +0300

    add allowed host

--- project/settings.py
+++ project/settings.py
@@ -12,11 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from decouple import config
 
 # General settings
 DEBUG = config('DEBUG', default=False, cast=bool)
